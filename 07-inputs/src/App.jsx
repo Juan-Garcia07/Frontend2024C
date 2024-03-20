3789efec2cfd5239581fe8aed804b0cc398dd4dc
Juan-Garcia07 <143968934+Juan-Garcia07@users.noreply.github.com>
2024-03-19 20:39:17 -0600

    updated

--- 07-inputs/src/App.jsx
+++ 07-inputs/src/App.jsx
@@ -1,16 +1,26 @@
 import { useState } from "react"
 
 function App() {
-  const [username, setUsername] = useState ("")
-const handleUsernameChange = (event) =>{
-  setUsername(event.target.value)
-  //console.log(username)
+  const [inputsForm, setInputsForm] = useState ({
+    username: "",
+    password: "",
+  })
+const handleInputChange = (event) =>{
+  setInputsForm({
+    ...inputsForm,
+    [event.target.name]: event.target.value,
+    
+  })
 
 }
 const handleSubmit = (e) =>{
   e.preventDefault()
-  console.log(username)
-  setUsername("")
+  console.log("Nombre de usuario:", inputsForm.username)
+  console.log("Contraseña:", inputsForm.password)
+  setInputsForm({
+    username: "",
+    password: "",
+ })
 }
 
   return (
@@ -23,12 +33,13 @@ const handleSubmit = (e) =>{
           <label htmlFor="username">Username</label>
           <input id="username"
            type="text"
-           value={username}
-           onChange={(event) => handleUsernameChange (event)} />
+           name="username"
+           value={inputsForm.username}
+           onChange={(event) => handleInputChange (event)} />
         </div>
         <div>
         <label htmlFor="password">Password</label>
-          <input id="password" type="password" />
+          <input id="password" type="password" name="password" value={inputsForm.password} onChange={(event) => handleInputChange(event)}/>
         </div>
         <button type="submit">Submit</button>
       </form>
